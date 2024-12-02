b328d06e2bec05a7742d4272d9a6da9eecd20cf4
merronmuche <merronmuche@gmail.com>
2024-12-02 22:43:54 +0300

    Add the Admin Configuration for models.

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
-from .models import County
+from app.models import MetropolitanArea, County, CensusTract, BlockGroup
+
+@admin.register(MetropolitanArea)
+class MetropolitanAreaAdmin(admin.ModelAdmin):
+    list_display = ("name", "cbsa_code")
+    search_fields = ("name", "cbsa_code")
+    list_filter = ("created_at", "updated_at")
+   
 
 @admin.register(County)
 class CountyAdmin(admin.ModelAdmin):
-    list_display = ["id","name", "fips_code"]
+    list_display = ("name", "metropolitan_area", "fips_code")
+    search_fields = ("name", "fips_code")
+    list_filter = ("metropolitan_area", "created_at", "updated_at")
+
+
+@admin.register(CensusTract)
+class CensusTractAdmin(admin.ModelAdmin):
+    list_display = ("name", "county", "fips_code")
+    search_fields = ("name", "fips_code")
+    list_filter = ("county", "created_at", "updated_at")
+
+
+@admin.register(BlockGroup)
+class BlockGroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "census_tract", "population", "male", "female", "median_income", "fips_code")
+    search_fields = ("name", "fips_code", "census_tract__name")
+    list_filter = ("census_tract", "created_at", "updated_at")
+    
