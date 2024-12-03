4f44cab8a63a5ec947dce7d565285ed2d900761f
merronmuche <merronmuche@gmail.com>
2024-12-03 17:57:18 +0300

    add metropolitan_area_county_mapping

---
+++ county_mapping.py
@@ -0,0 +1,212 @@
+metropolitan_area_county_mapping = {
+    "New York–Newark–Jersey City, NY-NJ-PA MSA": [
+        "Bronx, NY", "Kings, NY", "Nassau, NY", "New York, NY", "Queens, NY", 
+        "Richmond, NY", "Rockland, NY", "Suffolk, NY", "Westchester, NY",
+        "Bergen, NJ", "Essex, NJ", "Hudson, NJ", "Middlesex, NJ", "Monmouth, NJ",
+        "Morris, NJ", "Ocean, NJ", "Passaic, NJ", "Somerset, NJ", "Sussex, NJ", 
+        "Union, NJ", "Pike, PA"
+    ],
+    "Los Angeles–Long Beach–Anaheim, CA MSA": ["Los Angeles, CA", "Orange, CA"],
+    "Chicago–Naperville–Elgin, IL-IN-WI MSA": [
+        "Cook, IL", "DeKalb, IL", "DuPage, IL", "Grundy, IL", "Kane, IL", 
+        "Kendall, IL", "Lake, IL", "McHenry, IL", "Will, IL", 
+        "Jasper, IN", "Lake, IN", "Newton, IN", "Porter, IN", 
+        "Kenosha, WI"
+    ],
+    "Dallas–Fort Worth–Arlington, TX MSA": [
+        "Collin, TX", "Dallas, TX", "Denton, TX", "Ellis, TX", "Johnson, TX", 
+        "Kaufman, TX", "Parker, TX", "Tarrant, TX", "Wise, TX"
+    ],
+    "Houston–The Woodlands–Sugar Land, TX MSA": [
+        "Austin, TX", "Brazoria, TX", "Chambers, TX", "Fort Bend, TX", 
+        "Galveston, TX", "Harris, TX", "Liberty, TX", "Montgomery, TX", 
+        "Waller, TX"
+    ],
+    "Washington–Arlington–Alexandria, DC-VA-MD-WV MSA": [
+        "District of Columbia, DC", 
+        "Arlington, VA", "Clarke, VA", "Fairfax, VA", "Fauquier, VA", 
+        "Loudoun, VA", "Prince William, VA", "Spotsylvania, VA", 
+        "Stafford, VA", "Warren, VA",
+        "Calvert, MD", "Charles, MD", "Frederick, MD", "Montgomery, MD", 
+        "Prince George's, MD",
+        "Jefferson, WV"
+    ],
+    "Miami–Fort Lauderdale–West Palm Beach, FL MSA": [
+        "Broward, FL", "Miami-Dade, FL", "Palm Beach, FL"
+    ],
+    "Philadelphia–Camden–Wilmington, PA-NJ-DE-MD MSA": [
+        "Bucks, PA", "Chester, PA", "Delaware, PA", "Montgomery, PA", 
+        "Philadelphia, PA",
+        "Burlington, NJ", "Camden, NJ", "Gloucester, NJ", "Salem, NJ",
+        "New Castle, DE", "Cecil, MD"
+    ],
+    "Atlanta–Sandy Springs–Roswell, GA MSA": [
+        "Barrow, GA", "Bartow, GA", "Butts, GA", "Carroll, GA", "Cherokee, GA", 
+        "Clayton, GA", "Cobb, GA", "Coweta, GA", "Dawson, GA", "DeKalb, GA", 
+        "Douglas, GA", "Fayette, GA", "Forsyth, GA", "Fulton, GA", 
+        "Gwinnett, GA", "Haralson, GA", "Heard, GA", "Henry, GA", "Jasper, GA", 
+        "Lamar, GA", "Meriwether, GA", "Morgan, GA", "Newton, GA", "Paulding, GA", 
+        "Pickens, GA", "Pike, GA", "Rockdale, GA", "Spalding, GA", "Walton, GA"
+    ],
+    "Phoenix–Mesa–Chandler, AZ MSA": ["Maricopa, AZ", "Pinal, AZ"],
+    "Boston–Cambridge–Newton, MA-NH MSA": [
+        "Essex, MA", "Middlesex, MA", "Norfolk, MA", "Plymouth, MA", "Suffolk, MA", 
+        "Rockingham, NH", "Strafford, NH"
+    ],
+    "San Francisco–Oakland–Berkeley, CA MSA": [
+        "Alameda, CA", "Contra Costa, CA", "Marin, CA", "San Francisco, CA", 
+        "San Mateo, CA"
+    ],
+    "Riverside–San Bernardino–Ontario, CA MSA": [
+        "Riverside, CA", "San Bernardino, CA"
+    ],
+    "Detroit–Warren–Dearborn, MI MSA": [
+        "Lapeer, MI", "Livingston, MI", "Macomb, MI", "Oakland, MI", 
+        "St. Clair, MI", "Wayne, MI"
+    ],
+    "Seattle–Tacoma–Bellevue, WA MSA": ["King, WA", "Pierce, WA", "Snohomish, WA"],
+    "Minneapolis–St. Paul–Bloomington, MN-WI MSA": [
+        "Anoka, MN", "Carver, MN", "Chisago, MN", "Dakota, MN", "Hennepin, MN", 
+        "Isanti, MN", "Ramsey, MN", "Scott, MN", "Sherburne, MN", 
+        "Washington, MN", "Wright, MN", "Pierce, WI", "St. Croix, WI"
+    ],
+    "San Diego–Chula Vista–Carlsbad, CA MSA": ["San Diego, CA"],
+    "Tampa–St. Petersburg–Clearwater, FL MSA": [
+        "Hernando, FL", "Hillsborough, FL", "Pasco, FL", "Pinellas, FL"
+    ],
+    "Denver–Aurora–Lakewood, CO MSA": [
+        "Adams, CO", "Arapahoe, CO", "Broomfield, CO", "Clear Creek, CO", 
+        "Denver, CO", "Douglas, CO", "Elbert, CO", "Gilpin, CO", 
+        "Jefferson, CO", "Park, CO"
+    ],
+    "Baltimore–Columbia–Towson, MD MSA": [
+        "Anne Arundel, MD", "Baltimore, MD", "Carroll, MD", "Harford, MD", 
+        "Howard, MD", "Queen Anne's, MD", "Baltimore City, MD"
+    ],
+    "Orlando–Kissimmee–Sanford, FL MSA": [
+        "Lake, FL", "Orange, FL", "Osceola, FL", "Seminole, FL"
+    ],
+    "St. Louis, MO-IL MSA": [
+        "Bond, IL", "Calhoun, IL", "Clinton, IL", "Jersey, IL", "Macoupin, IL", 
+        "Madison, IL", "Monroe, IL", "St. Clair, IL", "Franklin, MO", 
+        "Jefferson, MO", "Lincoln, MO", "St. Charles, MO", "St. Louis, MO", 
+        "Warren, MO", "St. Louis City, MO"
+    ],
+    "Charlotte–Concord–Gastonia, NC-SC MSA": [
+        "Anson, NC", "Cabarrus, NC", "Gaston, NC", "Iredell, NC", "Lincoln, NC", 
+        "Mecklenburg, NC", "Rowan, NC", "Union, NC", "Chester, SC", "Lancaster, SC", 
+        "York, SC"
+    ],
+    "San Antonio–New Braunfels, TX MSA": [
+        "Atascosa, TX", "Bandera, TX", "Bexar, TX", "Comal, TX", "Guadalupe, TX", 
+        "Kendall, TX", "Medina, TX", "Wilson, TX"
+    ],
+    "Portland–Vancouver–Hillsboro, OR-WA MSA": [
+        "Clackamas, OR", "Columbia, OR", "Multnomah, OR", "Washington, OR", 
+        "Yamhill, OR", "Clark, WA", "Skamania, WA"
+    ],
+    "Sacramento–Roseville–Folsom, CA MSA": [
+        "El Dorado, CA", "Placer, CA", "Sacramento, CA", "Yolo, CA"
+    ],
+    "Las Vegas–Henderson–Paradise, NV MSA": [
+        "Clark, NV", "Nye, NV", "Lincoln, NV"
+    ],
+    "Austin–Round Rock–Georgetown, TX MSA": [
+        "Bastrop, TX", "Caldwell, TX", "Hays, TX", "Travis, TX", "Williamson, TX"
+    ],
+    "Indianapolis–Carmel–Anderson, IN MSA": [
+        "Boone, IN", "Brown, IN", "Hamilton, IN", "Hancock, IN", "Hendricks, IN", 
+        "Johnson, IN", "Madison, IN", "Marion, IN", "Morgan, IN", "Putnam, IN", 
+        "Shelby, IN"
+    ],
+    "Cincinnati, OH-KY-IN MSA": [
+        "Dearborn, IN", "Ohio, IN", "Union, IN", "Franklin, IN", "Boone, KY", 
+        "Bracken, KY", "Campbell, KY", "Gallatin, KY", "Grant, KY", "Kenton, KY", 
+        "Pendleton, KY", "Brown, OH", "Butler, OH", "Clermont, OH", "Hamilton, OH", 
+        "Warren, OH"
+    ],
+    "Kansas City, MO-KS MSA": [
+        "Johnson, KS", "Leavenworth, KS", "Linn, KS", "Miami, KS", "Wyandotte, KS", 
+        "Bates, MO", "Caldwell, MO", "Cass, MO", "Clay, MO", "Clinton, MO", 
+        "Jackson, MO", "Lafayette, MO", "Platte, MO", "Ray, MO"
+    ],
+    "Columbus, OH MSA": [
+        "Delaware, OH", "Fairfield, OH", "Franklin, OH", "Hocking, OH", 
+        "Licking, OH", "Madison, OH", "Morrow, OH", "Perry, OH", 
+        "Pickaway, OH", "Union, OH"
+    ],
+    "Cleveland–Elyria, OH MSA": [
+        "Cuyahoga, OH", "Geauga, OH", "Lake, OH", "Lorain, OH", "Medina, OH"
+    ],
+    "Nashville–Davidson–Murfreesboro–Franklin, TN MSA": [
+        "Cannon, TN", "Cheatham, TN", "Davidson, TN", "Dickson, TN", 
+        "Hickman, TN", "Maury, TN", "Montgomery, TN", "Robertson, TN", 
+        "Rutherford, TN", "Sumner, TN", "Williamson, TN", "Wilson, TN"
+    ],
+    "San Jose–Sunnyvale–Santa Clara, CA MSA": ["Santa Clara, CA"],
+    "Virginia Beach–Norfolk–Newport News, VA-NC MSA": [
+        "Currituck, NC", "Gates, NC", 
+        "Chesapeake City, VA", "Gloucester, VA", "Hampton City, VA", 
+        "Isle of Wight, VA", "James City, VA", "Mathews, VA", "Newport News City, VA", 
+        "Norfolk City, VA", "Poquoson City, VA", "Portsmouth City, VA", 
+        "Suffolk City, VA", "Virginia Beach City, VA", "Williamsburg City, VA", 
+        "York, VA"
+    ],
+    "Providence–Warwick, RI-MA MSA": [
+        "Bristol, RI", "Kent, RI", "Newport, RI", "Providence, RI", "Washington, RI", 
+        "Bristol, MA"
+    ],
+    "Milwaukee–Waukesha, WI MSA": [
+        "Milwaukee, WI", "Ozaukee, WI", "Washington, WI", "Waukesha, WI"
+    ],
+    "Raleigh–Cary, NC MSA": [
+        "Franklin, NC", "Johnston, NC", "Wake, NC"
+    ],
+    "Jacksonville, FL MSA": [
+        "Baker, FL", "Clay, FL", "Duval, FL", "Nassau, FL", "St. Johns, FL"
+    ],
+    "Richmond, VA MSA": [
+        "Amelia, VA", "Caroline, VA", "Charles City, VA", "Chesterfield, VA", 
+        "Dinwiddie, VA", "Goochland, VA", "Hanover, VA", "Henrico, VA", 
+        "King William, VA", "New Kent, VA", "Powhatan, VA", "Prince George, VA", 
+        "Sussex, VA", "Colonial Heights City, VA", "Hopewell City, VA", 
+        "Petersburg City, VA", "Richmond City, VA"
+    ],
+    "Oklahoma City, OK MSA": [
+        "Canadian, OK", "Cleveland, OK", "Grady, OK", "Lincoln, OK", 
+        "Logan, OK", "McClain, OK", "Oklahoma, OK"
+    ],
+    "Memphis, TN-MS-AR MSA": [
+        "Crittenden, AR", "DeSoto, MS", "Marshall, MS", "Tate, MS", 
+        "Tunica, MS", "Fayette, TN", "Shelby, TN", "Tipton, TN"
+    ],
+    "Salt Lake City, UT MSA": [
+        "Davis, UT", "Morgan, UT", "Salt Lake, UT", "Tooele, UT"
+    ],
+    "Louisville/Jefferson County, KY-IN MSA": [
+        "Bullitt County, KY", "Henry County, KY", "Jefferson County, KY", 
+        "Meade County, KY", "Oldham County, KY", "Shelby County, KY", 
+        "Spencer County, KY", "Trimble County, KY", "Clark County, IN", 
+        "Floyd County, IN", "Harrison County, IN"
+    ],
+    "Buffalo–Cheektowaga, NY MSA": [
+        "Erie County, NY", "Niagara County, NY"
+    ],
+    "Grand Rapids–Kentwood, MI MSA": [
+        "Barry County, MI", "Kent County, MI", "Montcalm County, MI", 
+        "Ottawa County, MI"
+    ],
+    "Fresno, CA MSA": [
+        "Fresno County, CA"
+    ],
+    "Birmingham–Hoover, AL MSA": [
+        "Bibb County, AL", "Blount County, AL", "Chilton County, AL", 
+        "Jefferson County, AL", "St. Clair County, AL", "Shelby County, AL", 
+        "Walker County, AL"
+    ]
+}
+
+    
+
+
+
