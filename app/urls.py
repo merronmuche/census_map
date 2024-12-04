1dfd76717810a7a49d635d732cdf78f2d969b37a
merronmuche <merronmuche@gmail.com>
2024-12-04 15:59:45 +0300

    refactor

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import (
     CensusTractGeoJSONView,
     CountyGeoJSONView,
-    create_counties_by_metro,
     metropolitan_map,
     get_metropolitan_areas,
 )
@@ -10,9 +9,6 @@ from .views import (
 urlpatterns = [
     path("api/get_metropolitan_areas/", get_metropolitan_areas, name="get_metropolitan_areas"),
     path("api/get_county_geojson/", CountyGeoJSONView.as_view(), name="county_geojson"),
-    path("county/create-by-metro/", create_counties_by_metro, name="create_counties_by_metro"),
-    path("map/", metropolitan_map, name="metropolitan_map"),
     path("api/get_census_tracts/", CensusTractGeoJSONView.as_view(), name="census_tract_geojson"),
-
-
+    path("map/", metropolitan_map, name="metropolitan_map"),
 ]
