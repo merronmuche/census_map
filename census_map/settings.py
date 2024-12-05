8907d08d3a4bd61ac1eb16b87c48d24387edadea
merronmuche <merronmuche@gmail.com>
2024-12-05 12:43:41 +0300

    retrieve the Census API Key from the environment

--- census_map/settings.py
+++ census_map/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Retrieve the Census API Key from the environment
+CENSUS_API_KEY = config("CENSUS_API_KEY")
+
