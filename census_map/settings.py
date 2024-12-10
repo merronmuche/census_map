893288d53de00603459d863f44189b2f513e08f7
merronmuche <merronmuche@gmail.com>
2024-12-10 19:52:45 +0300

    change the database from sqlite to postgres

--- census_map/settings.py
+++ census_map/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'census_map.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'postgres',  
+        'USER': 'postgres',  
+        'PASSWORD': 'password',  
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
 
